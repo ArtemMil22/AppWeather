cf64488c9410e905a0e2d6741a37e8a1d9587b6d
ArtemMil22 <bullfinch22@yandex.ru>
2023-03-18 15:56:49 +0400

    Add language RUS in app

--- app/src/main/java/com/example/myapplicationart/data/ApiService.kt
+++ app/src/main/java/com/example/myapplicationart/data/ApiService.kt
@@ -6,10 +6,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("forecast?&appid=2c5cac32b3272b5a7f972d948b9bc0dd&units=metric")
+    @GET("forecast?&lang=ru&appid=2c5cac32b3272b5a7f972d948b9bc0dd&units=metric")
     suspend fun getWeatherData(@Query("q") cityName:String): MainData
 
-    @GET ("forecast?&appid=2c5cac32b3272b5a7f972d948b9bc0dd&units=metric")
+    @GET ("forecast?&appid=2c5cac32b3272b5a7f972d948b9bc0dd")
     fun getDataRX(@Query("q") cityName:String): Observable<MainData>
 
 }
